221628fd1cc6b1bd57c2047e59a44fc24cab8fc1
Jose-Puente2001 <josegabrielpuente75@gmail.com>
2021-08-19 11:58:07 -0400

    styles app

--- index.php
+++ index.php
@@ -1,27 +1,22 @@
 <?php require_once 'db.php'; ?>
 
 <?php require_once 'includes/header.php'; ?>
-<main "container p-4">
+<main class="container p-4">
 <?php if (isset($_SESSION['message'])){ ?>
 <div class="alert alert-<?= $_SESSION['message_type']; ?> alert-dismissible fade show" role="alert">
   <?= $_SESSION['message']; ?>
   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
 <?php session_unset(); } ?>
-
+<div class="text-center">
 	<form action="save.php" method="POST">
 		<input type="text" name="name" placeholder="name" />
 		<input type="url" name="url" placeholder="url" />
-		<input type="submit" name="save_app">
+		<button class="btn btn-danger" name="save_app">Add Link</button>
 	</form>
+</div>
 </main>
-<?php 
-
-$query = "SELECT * FROM link";
-$result = mysqli_query($connection, $query);
-
-while($row = mysqli_fetch_array($result)){?>
-    <table class="table table-bordered table-hover">
+<table class="table table-bordered table-hover">
       	<thead>
       		<tr>
       			<th scope="col">Name</th>
@@ -30,16 +25,22 @@ while($row = mysqli_fetch_array($result)){?>
       			<th scope="col">Delete</th>
       		</tr>
       	</thead>
-      	<tbody>
+     <tbody>
+<?php 
+
+$query = "SELECT * FROM link";
+$result = mysqli_query($connection, $query);
+
+while($row = mysqli_fetch_array($result)){?>
       		<tr>
       			<th><?php echo $row['name']?></th>
       			<th><a href="<?php echo $row['link']?>" target="_blank"><?php echo $row['link']?></a></th>
-      			<th><a href="edit.php?id=<?php echo $row['id']?>">edit</a></th>
-      			<th><a href="delete.php?id=<?php echo $row['id']?>">delete</a></th>
+      			<th><a href="edit.php?id=<?php echo $row['id']?>"><i class="fas fa-edit"></i></a></th>
+      			<th><a href="delete.php?id=<?php echo $row['id']?>"><i class="fas fa-trash-alt"></i></a></th>
       		</tr>
-      	</tbody>
-      </table>
 <?php } ?>
+ 	</tbody>
+ </table>
 
 
 
